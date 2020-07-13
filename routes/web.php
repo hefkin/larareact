77efaa53e0166ffc9342d7566908bffd2fb3dea8
hefkin <blintsov2001@mail.ru>
2020-07-13 11:38:59 +0400

    second lesson

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\User;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -14,3 +15,9 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', 'IndexController@index');
+
+Route::get('/test', function() 
+{
+    $users = User::get();
+    dd($users);
+});
